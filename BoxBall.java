1fd79671a6250e3a4a316cfa324a0f391ef359f3
Aristolchia <G00231538@stu.raritanval.edu>
2015-03-23 12:47:35 -0400

    Copied and Edited BouncingBall- Mark1
    Added all 4 position variables

--- BoxBall.java
+++ BoxBall.java
@@ -1,4 +1,5 @@
-
+import java.awt.*;
+import java.awt.geom.*;
 /**
  * Write a description of class BoxBall here.
  * 
@@ -7,27 +8,93 @@
  */
 public class BoxBall
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private Ellipse2D.Double circle;
+    private Color color;
+    private int diameter;
+    private int xPosition;
+    private int yPosition;
+    private final int groundPosition;      // y position of ground
+    private final int topPosition;
+    private final int leftPosition;
+    private final int rightPosition;
+    private Canvas canvas;
+    private int ySpeed = 1;                // initial downward speed
+    private int xSpeed = 1;
 
-    /**
-     * Constructor for objects of class BoxBall
-     */
-    public BoxBall()
+    public BoxBall(int xPos, int yPos, int ballDiameter, Color ballColor, int groundPos, int topPos, int leftPos, int rightPos, Canvas drawingCanvas)
     {
-        // initialise instance variables
-        x = 0;
+        xPosition = xPos;
+        yPosition = yPos;
+        color = ballColor;
+        diameter = ballDiameter;
+        groundPosition = groundPos;
+        topPosition = topPos;
+        rightPosition = rightPos;
+        leftPosition = leftPos;
+        canvas = drawingCanvas;
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
+     * Draw this ball at its current position onto the canvas.
+     **/
+    public void draw()
     {
-        // put your code here
-        return x + y;
+        canvas.setForegroundColor(color);
+        canvas.fillCircle(xPosition, yPosition, diameter);
+    }
+
+    /**
+     * Erase this ball at its current position.
+     **/
+    public void erase()
+    {
+        canvas.eraseCircle(xPosition, yPosition, diameter);
+    }    
+
+    /**
+     * Move this ball according to its position and speed and redraw.
+     **/
+    public void move()
+    {
+        // remove from canvas at the current position
+        erase();
+            
+        // compute new position
+        yPosition += 2;
+        xPosition += 2;
+
+        // check if it has hit the ground
+        if(yPosition >= (groundPosition - diameter) && ySpeed > 0) {
+            yPosition = (int)(groundPosition - diameter);
+            ySpeed = -ySpeed;
+        }
+         if(yPosition >= (topPosition - diameter) && ySpeed > 0) {
+            yPosition = (int)(topPosition - diameter);
+            ySpeed = -ySpeed;
+        } if(yPosition >= (leftPosition - diameter) && ySpeed > 0) {
+            yPosition = (int)(leftPosition - diameter);
+            xSpeed = -xSpeed;
+        } if(yPosition >= (rightPosition - diameter) && ySpeed > 0) {
+            yPosition = (int)(rightPosition - diameter);
+            xSpeed = -xSpeed;
+        }
+        // draw again at new position
+        draw();
+    }    
+
+    /**
+     * return the horizontal position of this ball
+     */
+    public int getXPosition()
+    {
+        return xPosition;
+    }
+
+    /**
+     * return the vertical position of this ball
+     */
+    public int getYPosition()
+    {
+        return yPosition;
     }
 }
